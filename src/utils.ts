172de11c9225cbe8fee3cf044bf021efd7bd8edb
ElizabethT7 <tymofieievai@gmail.com>
2023-07-13 16:08:06 +0300

    fix: correct getName function

--- src/utils.ts
+++ src/utils.ts
@@ -5,5 +5,6 @@ export const generateCode = (function (start = 0) {
 export const getName = (str: string) => {
   const index = str.indexOf(' ');
   const name = str.slice(0, index + 2);
+  if (index === -1) return str;
   return name + '.';
 }
